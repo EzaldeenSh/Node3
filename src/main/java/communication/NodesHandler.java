43e995abcc1b17ef594a8a9aebde2aa42d7e5211
Ezaldeen <ezaldeenshoqairy@gmail.com>
2022-12-02 17:55:26 +0300

    02/12

--- src/main/java/communication/NodesHandler.java
+++ src/main/java/communication/NodesHandler.java
@@ -1,6 +1,5 @@
 package communication;
 import data.Node;
-import data.NodesDaoUser;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
@@ -8,12 +7,10 @@ import java.net.Socket;
 
 public class NodesHandler implements Runnable{
     private final Socket client;
-    private final NodesDaoUser nodesDaoUser;
     private final ObjectInputStream fromClient;
     public NodesHandler(Socket otherNode, ObjectInputStream fromClient){
         this.client = otherNode;
         this.fromClient = fromClient;
-        nodesDaoUser= NodesDaoUser.getInstance();
     }
     @Override
     public void run() {
